0cbc2f70cf219a56434d8f39c390f4a7d7dce513
MetaGamer <83955827+metaGamer61@users.noreply.github.com>
2024-01-30 20:46:13 +0100

    Removing inutilities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,3 +37,9 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "fr.segazium.kosiaInstaller.Main"
+    }
+}
